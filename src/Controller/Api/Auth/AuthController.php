b6916772db9c220de93616ac14d24d1e03702bdd
berkut1 <berkut1@gmail.com>
2024-04-03 04:26:54 +0300

    Api AuthController annotations to attributes

--- src/Controller/Api/Auth/AuthController.php
+++ src/Controller/Api/Auth/AuthController.php
@@ -3,46 +3,73 @@ declare(strict_types=1);
 
 namespace App\Controller\Api\Auth;
 
-use OpenApi\Annotations as OA;
+use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AuthController extends AbstractController
 {
-    /**
-     * @OA\Post(
-     *     path="/login/authentication_token",
-     *     tags={"Get JWT toket to Login"},
-     *     @OA\RequestBody(
-     *         @OA\JsonContent(
-     *             type="object",
-     *             required={"username", "password"},
-     *             @OA\Property(property="username", type="string"),
-     *             @OA\Property(property="password", type="string"),
-     *         ),
-     *     ),
-     *     @OA\Response(
-     *         response=200,
-     *         description="Success response",
-     *         @OA\JsonContent(
-     *             type="object",
-     *             @OA\Property(property="token", type="string"),
-     *         ),
-     *     ),
-     *     @OA\Response(
-     *         response=401,
-     *         description="Error",
-     *         @OA\JsonContent(
-     *              type="object",
-     *              @OA\Property(property="error", type="object", nullable=true,
-     *                  @OA\Property(property="code", type="integer"),
-     *                  @OA\Property(property="message", type="string"),
-     *              ),
-     *         ),
-     *     ),
-     * )
-     */
+//    /**
+//     * @OA\Post(
+//     *     path="/login/authentication_token",
+//     *     tags={"Get JWT toket to Login"},
+//     *     @OA\RequestBody(
+//     *         @OA\JsonContent(
+//     *             type="object",
+//     *             required={"username", "password"},
+//     *             @OA\Property(property="username", type="string"),
+//     *             @OA\Property(property="password", type="string"),
+//     *         ),
+//     *     ),
+//     *     @OA\Response(
+//     *         response=200,
+//     *         description="Success response",
+//     *         @OA\JsonContent(
+//     *             type="object",
+//     *             @OA\Property(property="token", type="string"),
+//     *         ),
+//     *     ),
+//     *     @OA\Response(
+//     *         response=401,
+//     *         description="Error",
+//     *         @OA\JsonContent(
+//     *              type="object",
+//     *              @OA\Property(property="error", type="object", nullable=true,
+//     *                  @OA\Property(property="code", type="integer"),
+//     *                  @OA\Property(property="message", type="string"),
+//     *              ),
+//     *         ),
+//     *     ),
+//     * )
+//     */
+    #[OA\Post(
+        path: '/login/authentication_token',
+        requestBody: new OA\RequestBody(
+            content: new OA\JsonContent(
+                required: ['username', 'password'],
+                properties: [
+                    new OA\Property(property: 'username', type: 'string'),
+                    new OA\Property(property: 'password', type: 'string'),
+                ],
+                type: 'object')),
+        tags: ['Get JWT toket to Login'],
+        responses: [
+            new OA\Response(response: 200, description: 'Success response',
+                content: new OA\JsonContent(
+                    properties: [new OA\Property(property: 'token', type: 'string')],
+                    type: 'object')),
+            new OA\Response(response: 401, description: 'Error',
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(property: 'error', properties: [
+                            new OA\Property(property: 'code', type: 'integer'),
+                            new OA\Property(property: 'message', type: 'string'),
+                        ], type: 'object', nullable: true),
+                    ],
+                    type: 'object')),
+        ])
+    ]
     #[Route('/login/authentication_token', name: 'authentication_token', methods: ['POST'])]
     public function login_check(): Response
     {
